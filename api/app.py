5509428bbbf07e18b82870cb83650b729d547a09
qstv <32876312+qstv@users.noreply.github.com>
2024-02-05 17:47:48 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -58,5 +58,6 @@ def handle_message(event):
             )
         )
 
-if __name__ == "__main__":
-  app.run()
+if __name__ == "__main__":	
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
